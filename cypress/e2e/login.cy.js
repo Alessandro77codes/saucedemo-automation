04fa89415a6d3b69e94862fa459f84653f7eebf1
Alessandro carvalhos santos <alessandrocarvalhosantos30@gmail.com>
2026-05-29 10:02:02 -0300

    refactor: isolar cy.visit no hook beforeEach para evitar duplicidade

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,18 @@
 describe('Login SauceDemo', () => {
 
+  // Executa esse comando automaticamente antes de cada "it" rodar
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
+
   // 1. Cenário: Sem usuário e sem senha
   it('Sem usuário e sem senha', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username is required')
   })
 
   // 2. Cenário: Login de sucesso (Caminho feliz)
   it('Login com sucesso', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
@@ -18,7 +21,6 @@ describe('Login SauceDemo', () => {
 
   // 3. Cenário: Senha incorreta
   it('Senha incorreta', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('senha_errada')
     cy.get('[data-test="login-button"]').click()
@@ -27,7 +29,6 @@ describe('Login SauceDemo', () => {
 
   // 4. Cenário: Digitar só a senha (sem usuário)
   it('Digitar só a senha', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username is required')
@@ -35,7 +36,6 @@ describe('Login SauceDemo', () => {
 
   // 5. Cenário: Só colocar o nome do usuário e não a senha
   it('Só colocar o nome do usuário', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Password is required')
@@ -43,7 +43,6 @@ describe('Login SauceDemo', () => {
 
   // 6. Cenário: Senha certa com usuário errado
   it('Senha certa com usuário errado', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('usuario_errado')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
